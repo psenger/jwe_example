38dbf53bce70309f9bf1cb99636661dbfd2297cd
Philip A Senger <philip.a.senger@cngrgroup.com>
2019-12-18 18:13:00 +1100

    * init

--- generate_rsa.sh
+++ generate_rsa.sh
@@ -2,5 +2,5 @@
 
 # when you generate a RSA key, the key has both the Private and Public Key in the encoded string.
 # the second call to openssl merely extracts the public key.
-openssl genrsa -out ./privkey.pem 2048
+openssl genrsa -aes256 -out ./privkey.pem 2048
 openssl rsa -in ./privkey.pem -pubout -out ./pubkey.pem
